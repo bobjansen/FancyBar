b47cd8ccdc0a45c88e2c848c1af3481a5b563db9
Bob Jansen <bobjansen@gmail.com>
2022-01-27 21:56:29 +0100

    Use globalVariables() to remove NOTE

--- R/FancyBar.R
+++ R/FancyBar.R
@@ -5,3 +5,22 @@ NULL
 .onLoad <- function(libname, pkgname) {
   options(FancyBar.AddSymbolName = TRUE)
 }
+
+utils::globalVariables(c(
+  '.',
+  'Group',
+  'Timestamp',
+  'Price',
+  'Size',
+  'Open',
+  'High',
+  'Low',
+  'Close',
+  'Volume',
+  'VWAP',
+  'TickCount',
+  '..tsName',
+  'ID',
+  'multiplier'
+))
+
